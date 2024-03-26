626a2abdb7409867ada2624b99eadae8d79663fa
jouleion <124351760+jouleion@users.noreply.github.com>
2024-03-26 09:35:58 +0100

    HATSA!

---
+++ train_model.py
@@ -0,0 +1,67 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+from helpers.tflite_c_converter import convert_tflite_to_c
+from helpers.function_generator import get_function_samples
+
+X, y = get_function_samples(samples=2000)
+
+# TODO: Split the data into training and testing sets below
+
+
+# Plotting training, testing, and validation data points in blue, yellow, and red respectively,
+# with corresponding labels 'Train', 'Test', 'Valid'
+plt.plot(X_train, y_train, 'b.', label="Train")
+plt.plot(X_test, y_test, 'y.', label="Test")
+plt.legend()
+plt.show()
+
+# TODO: Add the TensorFlow Keras Sequential model below
+
+
+# TODO: Add the model training (fitting) below
+
+
+# TODO: Optional > Uncomment the following lines to plot the training and validation history
+# # Extracting training and validation loss from training history
+# # Plotting training loss in green and validation loss in blue over epochs
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# x_range = range(1, len(history.epoch) + 1)
+# plt.plot(x_range, loss, 'g.', label='Training loss')
+# plt.plot(x_range, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
+
+# TODO: Uncomment the following lines to plot the actual data versus the predicted function
+# # Make predictions on the test set
+# predictions = model.predict(X_test)
+#
+# # Plot actual vs. predicted values
+# plt.clf()
+# plt.plot(X_test, y_test, 'b.', label='Actual')
+# plt.plot(X_test, predictions, 'r.', label='Predicted')
+# plt.legend()
+# plt.show()
+
+# TODO: Uncomment the following lines to save the TensorFlow model
+# # Define a path where models are saved
+# model_path = "models"
+#
+# # Export the trained model
+# model.export(model_path)
+
+# TODO: Convert the saved TensorFlow model to a TensorFlow Lite model
+
+
+# TODO: Optional > Implement a TensorFlow Lite Interpreter and check if the accuracy of the model stays the same
+
+
+# TODO: convert TensorFlow Lite model to a c-array for use on microcontrollers
+
